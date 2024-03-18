b3fb2af70648809cab56f4c5a41e50f26f5420a9
Francisco Viana <franciscosantos29@gmail.com>
2024-03-18 10:47:28 -0300

    first commit

--- rds/main.tf
+++ rds/main.tf
@@ -39,6 +39,6 @@ resource "aws_db_instance" "bd-soat3" {
   password               = "Agos1918"
   db_subnet_group_name   = aws_db_subnet_group.db-subnet.id
   vpc_security_group_ids = [aws_security_group.sg-rds.id]
-  publicly_accessible = true
-  skip_final_snapshot = true
+  publicly_accessible    = true
+  skip_final_snapshot    = true
 }
